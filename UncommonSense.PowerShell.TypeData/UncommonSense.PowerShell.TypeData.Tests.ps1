420fb9eb7042bddc298911e578d3876f3351405b
Jan Hoek <github@uncommonsense.nl>
2017-01-15 10:39:32 +0100

    More tests implemented

--- UncommonSense.PowerShell.TypeData/UncommonSense.PowerShell.TypeData.Tests.ps1
+++ UncommonSense.PowerShell.TypeData/UncommonSense.PowerShell.TypeData.Tests.ps1
@@ -1,4 +1,6 @@
-﻿Import-Module (Join-Path $PSScriptRoot 'UncommonSense.PowerShell.TypeData.psd1')
+﻿(Join-Path $PSScriptRoot 'UncommonSense.PowerShell.TypeData.psd1'),
+(Join-Path $PSScriptRoot 'bin/Debug/UncommonSense.PowerShell.TypeData.psd1') |
+    ForEach-Object { Import-Module $_ -ErrorAction SilentlyContinue } 
 
 Describe 'UncommonSense.Powershell.TypeData' {
 	It 'Correctly renders an AliasProperty' {
@@ -8,6 +10,14 @@ Describe 'UncommonSense.Powershell.TypeData' {
 			}
 		}) -replace '\s', '' | Should Be '<Types><Type><Name>Foo</Name><Members><AliasProperty><Name>Baz</Name><ReferencedMemberName>Bar</ReferencedMemberName></AliasProperty></Members></Type></Types>'
 	}
+
+    It 'Correctly renders a CodeMethod' {
+        (Types {
+            _Type Foo {
+                CodeMethod Baz (CodeReference Bar Qux)
+            }
+        }) -replace '\s', '' | Should Be '<Types><Type><Name>Foo</Name><Members><CodeMethod><Name>Baz</Name><CodeReference><TypeName>Bar</TypeName><MethodName>Qux</MethodName></CodeReference></CodeMethod></Members></Type></Types>'
+    }
 
 	It 'Correctly renders a NoteProperty' {
 		(Types { 
@@ -20,8 +30,10 @@ Describe 'UncommonSense.Powershell.TypeData' {
 	It 'Correctly renders a MemberSet' {
 		(Types { 
 			_Type Foo {
-				MemberSet Baz
+				MemberSet Baz {
+                    PropertySet DefaultDisplayPropertySet Bar,Qux,Quux
+                }
 			}
-		}) -replace '\s', '' | Should Be '<Types><Type><Name>Foo</Name><Members><MemberSet><Name>Baz</Name><Members/></MemberSet></Members></Type></Types>'
+		}) -replace '\s', '' | Should Be '<Types><Type><Name>Foo</Name><Members><MemberSet><Name>Baz</Name><Members><PropertySet><Name>DefaultDisplayPropertySet</Name><ReferencedProperties><Name>Bar</Name><Name>Qux</Name><Name>Quux</Name></ReferencedProperties></PropertySet></Members></MemberSet></Members></Type></Types>'
 	}
 }
