1b90445eabadd685fda5295f9b094ffcd5ee3f4e
Jan Hoek <github@uncommonsense.nl>
2017-01-15 10:39:17 +0100

    NewPropertySetCmdlet added

---
+++ UncommonSense.PowerShell.TypeData/Cmdlets/NewPropertySetCmdlet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UncommonSense.PowerShell.TypeData.Cmdlets
+{
+    [Cmdlet(VerbsCommon.New, "PropertySet")]
+    [OutputType(typeof(PropertySet))]
+    public class NewPropertySetCmdlet : NewMemberCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 1)]
+        public string[] ReferencedProperties
+        {
+            get; set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(new PropertySet(Name, ReferencedProperties));
+        }
+    }
+}
--- UncommonSense.PowerShell.TypeData/UncommonSense.PowerShell.TypeData.psm1
+++ UncommonSense.PowerShell.TypeData/UncommonSense.PowerShell.TypeData.psm1
@@ -7,5 +7,6 @@ Set-Alias -Name CodeProperty -Value New-CodeProperty
 Set-Alias -name CodeReference -Value New-CodeReference
 Set-Alias -Name MemberSet -Value New-MemberSet
 Set-Alias -Name NoteProperty -Value New-NoteProperty
+Set-Alias -Name PropertySet -Value New-PropertySet
 Set-Alias -Name ScriptMethod -Value New-ScriptMethod
 Set-Alias -Name ScriptProperty -Value New-ScriptProperty
