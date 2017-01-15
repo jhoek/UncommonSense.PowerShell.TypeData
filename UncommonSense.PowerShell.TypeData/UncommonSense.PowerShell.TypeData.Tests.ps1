cb31c492a6fc6bc0722cc336f438f4859735e9a6
Jan Hoek <github@uncommonsense.nl>
2017-01-15 09:35:06 +0100

    First few automated tests

--- UncommonSense.PowerShell.TypeData/UncommonSense.PowerShell.TypeData.Tests.ps1
+++ UncommonSense.PowerShell.TypeData/UncommonSense.PowerShell.TypeData.Tests.ps1
@@ -1,7 +1,27 @@
-﻿Import-Module (Join-Path $PSScriptRoot 'bin\debug\UncommonSense.PowerShell.TypeData.psd1')
+﻿Import-Module (Join-Path $PSScriptRoot 'UncommonSense.PowerShell.TypeData.psd1')
 
 Describe 'UncommonSense.Powershell.TypeData' {
+	It 'Correctly renders an AliasProperty' {
+		(Types {
+			_Type Foo {
+				AliasProperty Baz Bar
+			}
+		}) -replace '\s', '' | Should Be '<Types><Type><Name>Foo</Name><Members><AliasProperty><Name>Baz</Name><ReferencedMemberName>Bar</ReferencedMemberName></AliasProperty></Members></Type></Types>'
+	}
+
 	It 'Correctly renders a NoteProperty' {
-		Types { Type Foo { NoteProperty Baz Bar } } 
+		(Types { 
+			_Type Foo { 
+				NoteProperty Baz Bar
+			} 
+		}) -replace '\s', '' | Should Be '<Types><Type><Name>Foo</Name><Members><NoteProperty><Name>Baz</Name><Value>Bar</Value></NoteProperty></Members></Type></Types>'
+	}
+
+	It 'Correctly renders a MemberSet' {
+		(Types { 
+			_Type Foo {
+				MemberSet Baz
+			}
+		}) -replace '\s', '' | Should Be '<Types><Type><Name>Foo</Name><Members><MemberSet><Name>Baz</Name><Members/></MemberSet></Members></Type></Types>'
 	}
 }
