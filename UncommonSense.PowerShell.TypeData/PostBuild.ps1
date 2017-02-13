445eb6ac3f455ff5b031d8987b70daed4656390b
Jan Hoek <github@uncommonsense.nl>
2017-02-13 20:20:37 +0100

    Use a PowerShell script to copy project output to modules folder. Closes #1.

---
+++ UncommonSense.PowerShell.TypeData/PostBuild.ps1
@@ -0,0 +1,15 @@
+﻿$ModulesFolder = ($env:PSModulePath -split ';')[0]
+
+$FromFolder = Join-Path -Path $PSScriptRoot -ChildPath 'bin/debug'
+$ToFolder = Join-Path -Path $ModulesFolder -ChildPath 'UncommonSense.PowerShell.TypeData/'
+
+if (-not (Test-Path -Path $ToFolder))
+{
+    New-Item -Path $ToFolder -ItemType Directory -ErrorAction SilentlyContinue
+}
+
+Copy-Item `
+    -Path $FromFolder\UncommonSense.PowerShell.TypeData.* `
+    -Exclude *.pdb `
+    -Destination $ToFolder `
+	-Force
