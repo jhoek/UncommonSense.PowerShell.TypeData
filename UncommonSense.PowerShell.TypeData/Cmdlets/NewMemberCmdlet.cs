ee5340c262ace878a3823c5b67133332a95da23d
Jan Hoek <github@uncommonsense.nl>
2018-03-07 11:09:21 +0100

    Added more xml documentation to trigger a new AppVeyor build

--- UncommonSense.PowerShell.TypeData/Cmdlets/NewMemberCmdlet.cs
+++ UncommonSense.PowerShell.TypeData/Cmdlets/NewMemberCmdlet.cs
@@ -9,6 +9,10 @@ namespace UncommonSense.PowerShell.TypeData.Cmdlets
 {
     public abstract class NewMemberCmdlet : Cmdlet
     {
+        /// <summary>
+        /// <para type="description">Name of this member</para>
+        /// </summary>
+        /// <returns></returns>
         [Parameter(Mandatory = true, Position =0)]
         public string Name
         {
